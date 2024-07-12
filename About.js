95d462e6661bfbfd4df19cedced6d603dbd73215
UsmanganiYSR <gani52194@gmail.com>
2024-07-12 12:30:59 +0530

    usmanportfolio

---
+++ About.js
@@ -0,0 +1,15 @@
+// src/pages/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>Name:usmangani</p>
+      <p>Email:gani52194@gmail.com</p>
+      <p>Mobile:9441009588</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,15 @@
+// src/pages/Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <p>Please feel free to reach out to me via email or phone.</p>
+      <p>Email:gani52194@gmail.com</p>
+      <p>Phone:9441009588</p>
+    </div>
+  );
+}
+
+export default Contact;
